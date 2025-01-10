c5e8c004491553e5c44c12e4b60bb61ec7d26019
Samuel Gomes Brandao <gb.samuel@gmail.com>
2025-01-10 16:07:05 -0300

    Reorganize class methods using class << syntax to enable private methods

--- lib/tabasco/section.rb
+++ lib/tabasco/section.rb
@@ -9,126 +9,127 @@ module Tabasco
 
     private_class_method :new # Use .load instead (or .visit for Page objects)
 
-    def self.load(...)
-      new(...).tap(&:ensure_loaded)
-    end
-
-    class_attribute :_attributes
-
-    def self.attribute(attr_name)
-      attributes << attr_name.to_sym
-      attr_reader attributes.last
-    end
-
-    # class_attribute does not play nicely with a mutable array as default value
-    def self.attributes
-      self._attributes ||= []
-    end
-
-    class_attribute :test_id
-
-    def self.container_test_id(test_id)
-      self.test_id = test_id.to_s.tr("_", "-")
-    end
-
-    # rubocop: disable Metrics/MethodLength
-    # rubocop: disable Metrics/PerceivedComplexity
-    def self.section(name, klass = nil, test_id: nil, &)
-      define_inline_section(name, klass, test_id: test_id, &)
-    end
-
-    def self.portal(name, local_concrete_klass = nil, &)
-      Tabasco.configuration.portal(name) in { klass:, test_id: }
-
-      define_inline_section(name, klass, test_id:, portal: true, &)
-    end
-
-    def self.define_inline_section(name, klass = nil, test_id: nil, portal: false, &block)
-      test_id = (test_id || name).to_s.tr("_", "-")
-
-      parent_attributes = attributes
-
-      # Inherit from a concrete klass, so inline definitions can override behavior
-      # without messing the actual concrete class
-      klass = Class.new(klass) if klass
-      klass ||= Class.new(Section) do
-        parent_attributes.each do |attr_name|
-          attribute attr_name
-        end
-
-        def self.attribute(*)
-          raise ArgumentError, "Attributes cannot be defined in anonymous sections. " \
-                               "They inherit all arguments from parent pages/sections automatically."
-        end
-
-        # Simple and naive implementation for Anonymous classes
-        # Will raise a Capybara error if the container cannot be found
-        # Can be overridden in the inline block anyway
-        ensure_loaded { container }
+    class_attribute :_attributes, :test_id
+    class << self
+      def load(...)
+        new(...).tap(&:ensure_loaded)
       end
 
-      # Configurations we want to apply to both inline or concrete classes
-      klass.class_eval do
-        # Although concrete classes specify a container test id, we want the local context
-        # to have a higher precedence.
-        container_test_id(test_id)
-
-        klass.class_eval(&block) if block
+      def attribute(attr_name)
+        attributes << attr_name.to_sym
+        attr_reader attributes.last
       end
 
-      define_method(name) do |&block_argument|
-        instance = instance_variable_get(:"@#{name}")
-        unless instance
-          inherited_attributes = self.class.attributes.each_with_object({}) do |attr_name, hash|
-            hash[attr_name] = public_send(attr_name)
+      # class_attribute does not play nicely with a mutable array as default value
+      def attributes
+        self._attributes ||= []
+      end
+
+      def container_test_id(test_id)
+        self.test_id = test_id.to_s.tr("_", "-")
+      end
+
+      def section(name, klass = nil, test_id: nil, &)
+        define_inline_section(name, klass, test_id: test_id, &)
+      end
+
+      def portal(name, local_concrete_klass = nil, &)
+        Tabasco.configuration.portal(name) in { klass:, test_id: }
+
+        define_inline_section(name, klass, test_id:, portal: true, &)
+      end
+
+      private
+
+      # rubocop: disable Metrics/MethodLength
+      # rubocop: disable Metrics/PerceivedComplexity
+      def define_inline_section(name, klass = nil, test_id: nil, portal: false, &block)
+        test_id = (test_id || name).to_s.tr("_", "-")
+
+        parent_attributes = attributes
+
+        # Inherit from a concrete klass, so inline definitions can override behavior
+        # without messing the actual concrete class
+        klass = Class.new(klass) if klass
+        klass ||= Class.new(Section) do
+          parent_attributes.each do |attr_name|
+            attribute attr_name
           end
 
-          # Filter attributes to only include those defined in the explicit class
-          filtered_attributes = inherited_attributes.slice(*klass.attributes)
+          def self.attribute(*)
+            raise ArgumentError, "Attributes cannot be defined in anonymous sections. " \
+                                 "They inherit all arguments from parent pages/sections automatically."
+          end
 
-          instance = klass.load(name, _parent_scope: portal ? nil : self, **filtered_attributes)
-          instance_variable_set(:"@#{name}", instance)
+          # Simple and naive implementation for Anonymous classes
+          # Will raise a Capybara error if the container cannot be found
+          # Can be overridden in the inline block anyway
+          ensure_loaded { container }
         end
 
-        block_argument&.call(instance)
-        instance
+        # Configurations we want to apply to both inline or concrete classes
+        klass.class_eval do
+          # Although concrete classes specify a container test id, we want the local context
+          # to have a higher precedence.
+          container_test_id(test_id)
+
+          klass.class_eval(&block) if block
+        end
+
+        define_method(name) do |&block_argument|
+          instance = instance_variable_get(:"@#{name}")
+          unless instance
+            inherited_attributes = self.class.attributes.each_with_object({}) do |attr_name, hash|
+              hash[attr_name] = public_send(attr_name)
+            end
+
+            # Filter attributes to only include those defined in the explicit class
+            filtered_attributes = inherited_attributes.slice(*klass.attributes)
+
+            instance = klass.load(name, _parent_scope: portal ? nil : self, **filtered_attributes)
+            instance_variable_set(:"@#{name}", instance)
+          end
+
+          block_argument&.call(instance)
+          instance
+        end
       end
-    end
-    # rubocop: enable Metrics/MethodLength
-    # rubocop: enable Metrics/PerceivedComplexity
+      # rubocop: enable Metrics/MethodLength
+      # rubocop: enable Metrics/PerceivedComplexity
 
-    # Automatically adds a private precondition method for any has_X? query method added.
-    # If a method named `has_<something>?` is defined, we will create a corresponding
-    # `has_<something>!` method.
-    #
-    # The `has_X!` method will call the original `has_X?` method and raise a `PreconditionNotMetError`
-    # if the result is not truthy.
-    def self.method_added(method_name)
-      method_name = method_name.to_s
+      # Automatically adds a private precondition method for any has_X? query method added.
+      # If a method named `has_<something>?` is defined, we will create a corresponding
+      # `has_<something>!` method.
+      #
+      # The `has_X!` method will call the original `has_X?` method and raise a `PreconditionNotMetError`
+      # if the result is not truthy.
+      def method_added(method_name)
+        method_name = method_name.to_s
 
-      return unless method_name.start_with?("has_") && method_name.end_with?("?")
+        return unless method_name.start_with?("has_") && method_name.end_with?("?")
 
-      bang_method_name = method_name.to_s.sub("?", "!")
+        bang_method_name = method_name.to_s.sub("?", "!")
 
-      return if instance_methods.include?(bang_method_name.to_sym)
+        return if instance_methods.include?(bang_method_name.to_sym)
 
-      original_method = instance_method(method_name)
+        original_method = instance_method(method_name)
 
-      define_method(bang_method_name) do |*args, **kwargs, &block|
-        result = original_method.bind_call(self, *args, **kwargs, &block)
+        define_method(bang_method_name) do |*args, **kwargs, &block|
+          result = original_method.bind_call(self, *args, **kwargs, &block)
 
-        return result if result
+          return result if result
 
-        raise(
-          PreconditionNotMetError,
-          "#{bang_method_name}: Expected #{method_name} to return truthy, but it returned #{result.inspect}",
-        )
+          raise(
+            PreconditionNotMetError,
+            "#{bang_method_name}: Expected #{method_name} to return truthy, but it returned #{result.inspect}",
+          )
+        end
+
+        # We don't want to expose the bang method to the outside world
+        private bang_method_name
+
+        super
       end
-
-      # We don't want to expose the bang method to the outside world
-      private bang_method_name
-
-      super
     end
 
     attr_reader :_handle
